673a096e4fa9b76e4489c7b635776c59ab1bd24c
Gabriel Almeida <ggalmeida1@users.noreply.github.com>
2021-07-13 00:20:10 -0300

    intial commit

--- utils/db.js
+++ utils/db.js
@@ -2,5 +2,5 @@ const faunadb = require('faunadb')
 export const q = faunadb.query
 
 export const client = new faunadb.Client({
-  secret: ''
+  secret: 'fnAEN-oF0xACQ_Dx3tZJO-M6etTymJFhNQKlLlp'
 })
